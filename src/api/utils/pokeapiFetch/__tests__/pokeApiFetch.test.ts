16332c95648d9d9145235a83989e2039ba5ad998
Diego Sanz <diego2sanz@gmail.com>
2022-11-28 17:40:17 +0100

    Add pokeApiFetch

---
+++ src/api/utils/pokeapiFetch/__tests__/pokeApiFetch.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, vi } from 'vitest'
+import pokeApiFetch from '../pokeApiFetch'
+
+describe('pokeApiFetch', () => {
+  global.fetch = vi.fn(() => Promise.resolve(true) as unknown as any)
+
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should call endpoint with the base url prefix', () => {
+    pokeApiFetch('test')
+    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(fetch).toBeCalledWith('https://pokeapi.co/api/v2/test', undefined)
+  })
+
+  it('should pass `init` parameters', () => {
+    pokeApiFetch('test', { method: 'POST' })
+    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(fetch).toBeCalledWith('https://pokeapi.co/api/v2/test', {
+      method: 'POST',
+    })
+  })
+})
