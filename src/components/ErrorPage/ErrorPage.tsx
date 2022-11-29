10f1051a5675cd23ea800446848607d1ba760b3c
Diego Sanz <diego2sanz@gmail.com>
2022-11-30 00:26:50 +0100

    Styles for 404

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 import notFoundImg from '../../assets/img/404.jpg'
+import styles from './ErrorPage.module.scss'
 
 const ErrorPage: FC = () => {
   return (
-    <div>
+    <div className={styles.errorPage}>
       <img src={notFoundImg} alt="404" />
+
+      <Link to={'/'} className={styles.homeLink}>
+        Go to the main page
+      </Link>
     </div>
   )
 }
