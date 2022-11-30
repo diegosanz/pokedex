f8cce2803f20008ce3a909f42d3b15ab6d52cdc7
Diego Sanz <diego2sanz@gmail.com>
2022-11-30 11:14:52 +0100

    Test pagination buttons

--- cypress/e2e/pokemon-pagination.cy.ts
+++ cypress/e2e/pokemon-pagination.cy.ts
@@ -61,4 +61,28 @@ describe('view pokemon detail', () => {
         expect($el.text()).to.equal(name)
       })
   })
+
+  it('disable pagination buttons when there are no more items', () => {
+    cy.intercept('https://pokeapi.co/api/v2/pokemon?offset=0&limit=15', {
+      body: {
+        count: 3,
+        next: null,
+        previous: null,
+        results: [
+          { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+          { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+          { name: 'venusaur', url: 'https://pokeapi.co/api/v2/pokemon/3/' },
+        ],
+      },
+    })
+
+    // Visit index
+    cy.visit('/')
+
+    // Navigate next disabled
+    cy.get(`[aria-label="Next page"]`).should('be.disabled')
+
+    // Navitage previous disabled
+    cy.get(`[aria-label="Previous page"]`).should('be.disabled')
+  })
 })
